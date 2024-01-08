06ee63b043632b1ef674a4edbbaf47a99304378b
Faiyaz <faiyazansari0010@gmail.com>
2024-01-08 12:30:48 +0530

    Adding Problem and solutions

---
+++ Data Structures and Algorithms in C++/OOP 1/Complex Number Class.cpp
@@ -0,0 +1,61 @@
+/**********
+ 
+Following is the main function we are using internally.
+Refer this for completing the ComplexNumbers class.
+ 
+ 
+int main() {
+    int real1, imaginary1, real2, imaginary2;
+    
+    cin >> real1 >> imaginary1;
+    cin >> real2 >> imaginary2;
+    
+    ComplexNumbers c1(real1, imaginary1);
+    ComplexNumbers c2(real2, imaginary2);
+    
+    int choice;
+    cin >> choice;
+    
+    if(choice == 1) {
+        c1.plus(c2);
+        c1.print();
+    }
+    else if(choice == 2) {
+        c1.multiply(c2);
+        c1.print();
+    }
+    else {
+        return 0;
+    }
+    
+}
+ 
+ ************/
+
+
+class ComplexNumbers {
+    private:
+    int real, imaginary;
+    
+    public:
+    ComplexNumbers(int r, int imag){
+        real = r;
+        imaginary = imag;
+    }
+    
+    void print(){
+        cout<< real << " + " << 'i' << imaginary << endl;
+    }
+    
+    void plus(ComplexNumbers c2){
+        real = real + c2.real;
+        imaginary = imaginary + c2.imaginary;
+    }
+    
+    void multiply(ComplexNumbers c2){
+        int r = real*c2.real + (-1 * imaginary * c2.imaginary);
+        int img = (real*c2.imaginary) + (imaginary*c2.real);
+        real=r;
+        imaginary=img;
+    }
+};
