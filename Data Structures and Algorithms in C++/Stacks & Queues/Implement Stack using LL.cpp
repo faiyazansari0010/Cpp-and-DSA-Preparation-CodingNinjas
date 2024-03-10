e6c144466cb9fd99de20b7635da2814c9349ccd3
Faiyaz <faiyazansari0010@gmail.com>
2024-03-10 12:34:50 +0530

    Adding Problem and Solution

---
+++ Data Structures and Algorithms in C++/Stacks & Queues/Implement Stack using LL.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Stack {
+	// Define the data members
+    Node *head;
+    int size;
+   public:
+    Stack() {
+        // Implement the Constructor
+        this->head=NULL;
+        this->size=0;
+    }
+
+	/*----------------- Public Functions of Stack -----------------*/
+
+    int getSize() {
+        // Implement the getSize() function
+        return size;
+    }
+
+    bool isEmpty() {
+        // Implement the isEmpty() function
+        return size==0;
+    }
+
+    void push(int element) {
+        // Implement the push() function
+        Node *newNode = new Node(element);
+        if(head==NULL){
+            head=newNode;
+        }
+        else{
+        newNode->next=head;
+        head=newNode;
+        }
+        size++;
+    }
+
+    int pop() {
+        // Implement the pop() function
+        if(head==NULL){
+            return -1;
+        }
+        int ans = head->data;
+        Node *temp=head;
+        head=head->next;
+        size--;
+        delete temp;
+        return ans;
+    }
+
+    int top() {
+        // Implement the top() function
+        if(head==NULL){
+            return -1;
+        }
+        return head->data;
+    }
+};
+
+int main(){
+  
+  return 0;
+}
