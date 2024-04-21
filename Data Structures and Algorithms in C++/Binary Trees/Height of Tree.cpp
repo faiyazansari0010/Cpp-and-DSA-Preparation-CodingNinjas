c1e5ded7334b9ac0f3eed64f3da0ec2b9375a4b5
Faiyaz <faiyazansari0010@gmail.com>
2024-04-21 14:40:49 +0530

    Adding Problem and Solution

---
+++ Data Structures and Algorithms in C++/Binary Trees/Height of Tree.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int main(){
+  
+  int height(BinaryTreeNode<int>* root) {
+    if(root == NULL){
+        return 0;
+    }
+    
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    
+    int h = max(leftHeight, rightHeight);
+    return 1 + h;
+}
+
+  return 0;
+}
