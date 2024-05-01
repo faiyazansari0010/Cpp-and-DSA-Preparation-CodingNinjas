278c576859f4d090d223103fac5a48c1eac7043e
Faiyaz <faiyazansari0010@gmail.com>
2024-05-01 15:37:40 +0530

    Adding Problem and Solution

---
+++ Data Structures and Algorithms in C++/Binary Search Tree/Pair Sum Binary Tree.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+#include <string>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode
+{
+public:
+  T data;
+  BinaryTreeNode<T> *left;
+  BinaryTreeNode<T> *right;
+
+  BinaryTreeNode(T data)
+  {
+    this->data = data;
+    left = NULL;
+    right = NULL;
+  }
+};
+
+void populate(BinaryTreeNode<int> *root, vector<int> &data)
+{
+  if (root == NULL)
+  {
+    return;
+  }
+
+  data.push_back(root->data);
+  populate(root->left, data);
+  populate(root->right, data);
+}
+
+void pairSum(BinaryTreeNode<int> *root, int sum)
+{
+
+  vector<int> data;
+
+  populate(root, data);
+
+  sort(data.begin(), data.end());
+
+  int i = 0, j = data.size() - 1;
+
+  while (i < j)
+  {
+    if (data[i] + data[j] == sum)
+    {
+      cout << data[i] << " " << data[j] << endl;
+      ++i;
+      --j;
+    }
+
+    else if (data[i] + data[j] > sum)
+    {
+      --j;
+    }
+
+    else
+    {
+      ++i;
+    }
+  }
+}
+
+int main()
+{
+
+  return 0;
+}
