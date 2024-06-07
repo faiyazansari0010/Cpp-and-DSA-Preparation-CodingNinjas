b5c9a37e39f77921860f6e49931bcb8809da6eb0
Faiyaz <faiyazansari0010@gmail.com>
2024-06-07 13:47:04 +0530

    Adding Problem and Solution

---
+++ Data Structures and Algorithms in C++/Dynamic Programming - 2/Edit Distance.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+using namespace std;
+
+// Recursion
+
+// int editDistance(string s1, string s2) {
+	
+//     if(s1.size() == 0 or s2.size() == 0){
+//         return max(s1.size(), s2.size());
+//     }
+    
+//     int ans;
+//     if(s1[0] == s2[0]){
+//         ans = editDistance(s1.substr(1), s2.substr(1));
+//     }
+    
+//     else{
+//         int ans1 = 1 + editDistance(s1.substr(1), s2);
+//         int ans2 = 1 + editDistance(s1, s2.substr(1));
+//         int ans3 = 1 + editDistance(s1.substr(1), s2.substr(1));
+        
+//         ans = min(ans1, min(ans2, ans3));
+//     }
+//     return ans;
+// }
+
+
+// Memoization
+
+int editDistance(string s1, string s2, int** output){
+    if(s1.size() == 0 or s2.size() == 0){
+        return max(s1.size(), s2.size());
+    }
+    
+    int m = s1.size(), n = s2.size();
+    
+    if(output[m][n] != -1){
+        return output[m][n];
+    }
+    
+    if(s1[0] == s2[0]){
+        output[m][n] = editDistance(s1.substr(1), s2.substr(1), output);
+    }
+    
+    else{
+        int ans1 = 1 + editDistance(s1.substr(1), s2, output);
+        int ans2 = 1 + editDistance(s1, s2.substr(1), output);
+        int ans3 = 1 + editDistance(s1.substr(1), s2.substr(1), output);
+        
+        output[m][n] = min(ans1, min(ans2, ans3));
+    }
+    return output[m][n];
+}
+
+int editDistance(string s1, string s2) {
+    
+    int m = s1.size()+1;
+    int n = s2.size()+1;
+    
+    int **output = new int*[m];
+    for(int i=0;i<=m;i++){
+        output[i] = new int[n];
+        for(int j=0;j<=n;j++){
+            output[i][j] = -1;
+        }
+    }
+    
+    return editDistance(s1, s2, output);
+}
+
+
+// Dynamic Programming
+
+int editDistance(string s1, string s2) {
+    
+	int m = s1.size();
+    int n = s2.size();
+    
+    int **output = new int*[m];
+    for(int i=0;i<=m;i++){
+        output[i] = new int[n];
+        for(int j=0;j<=n;j++){
+            output[i][j] = -1;
+        }
+    }
+    
+    for(int i=0;i<=m;i++){
+        output[0][i] = i;
+    }
+    
+    for(int i=0;i<=n;i++){
+        output[i][0] = i;
+    }
+    
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(s1[])
+        }
+    }
+}
+
+int main(){
+  
+  return 0;
+}
